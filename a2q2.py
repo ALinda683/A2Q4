d614d2911baf9b7a9f85ab0d39a9a28d5cff9c00
ALinda683 <ugj683@usask.ca>
2023-06-18 08:10:21 -0600

    WhiteBox testing: A whitebox test function is created when functions that comes under the white box testing and is being called.

--- a2q2.py
+++ a2q2.py
@@ -227,3 +227,11 @@ def test_sort_students_into_grades():
         print("PASS: Student list with valid and invalid grades")
     else:
         print("FAIL: Student list with valid and invalid grades")
+
+
+def test_whitebox_functions():
+    test_gcd()
+    test_replace()
+    test_grade_letter()
+
+
