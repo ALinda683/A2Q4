8403e9de307b625c0e1300560c39b5c07bc59c11
ALinda683 <ugj683@usask.ca>
2023-06-18 04:08:33 -0600

    test cases are added for the 'sort_students_into_grades' function.

--- a2q2.py
+++ a2q2.py
@@ -188,3 +188,42 @@ def test_grade_letter():
             print(f"PASS: grade_letter({score}) returned '{result}'")
         else:
             print(f"FAIL: grade_letter({score}) returned '{result}', expected '{expected}'")
+
+
+def test_sort_students_into_grades():
+    # Test pre-condition: Empty student list
+    student_list = []
+    result = sort_students_into_grades(student_list)
+    expected = {
+        "A": [],
+        "B": [],
+        "C": [],
+        "D": [],
+        "F": [],
+        "Invalid": []
+    }
+    if result == expected:
+        print("PASS: Empty student list")
+    else:
+        print("FAIL: Empty student list")
+
+    # Test post-conditions and return value
+    student_list = [
+        {"name": "John", "grade": "A"},
+        {"name": "Emily", "grade": "B"},
+        {"name": "Michael", "grade": "C"},
+        {"name": "Sarah"}
+    ]
+    result = sort_students_into_grades(student_list)
+    expected = {
+        "A": ["John"],
+        "B": ["Emily"],
+        "C": ["Michael"],
+        "D": [],
+        "F": [],
+        "Invalid": ["Sarah"]
+    }
+    if result == expected:
+        print("PASS: Student list with valid and invalid grades")
+    else:
+        print("FAIL: Student list with valid and invalid grades")
